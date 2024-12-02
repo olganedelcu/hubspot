f82b5452121c2b484a1ea785d82cd9d99825342e
Olga <olganedelcuam@gmail.com>
2024-12-02 23:50:15 +0100

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,10 +8,9 @@ const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />  {/* Home route */}
-        <Route path="/hubspot" element={<HubSpotIntegration />} />  {/* HubSpot integration route */}
-        <Route path="/hubspot/callback" element={<HubSpotCallback />} />  {/* HubSpot OAuth callback route */}
-        {/* Add more routes as needed */}
+        <Route path="/" element={<Home />} />  
+        <Route path="/hubspot" element={<HubSpotIntegration />} />  
+        <Route path="/hubspot/callback" element={<HubSpotCallback />} />
       </Routes>
     </Router>
   );
